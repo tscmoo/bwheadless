e7aa9ce58437ea19e49bf16d29cf82f73ec204a2
tscmoo <the.dengus@gmail.com>
2016-06-30 17:53:43 +0200

    sc_hook: Added remote hooking capability, for creating hooks in remote processes

--- sc_hook.cpp
+++ sc_hook.cpp
@@ -4,34 +4,35 @@
 #include "x86dec.h"
 
 #include "sc_hook.h"
-int hookflag_regs[8] = {hookflag_eax,hookflag_ecx,hookflag_edx,hookflag_ebx,hookflag_esp,hookflag_ebp,hookflag_esi,hookflag_edi};
+int hookflag_regs[8] = { hookflag_eax,hookflag_ecx,hookflag_edx,hookflag_ebx,hookflag_esp,hookflag_ebp,hookflag_esi,hookflag_edi };
 
-
-void xcept(const char*, ...) {
-	std::terminate();
-}
-
-void*hook_generate_entry(void*address,void*out_ins,size_t*out_size) {
+void* hook_generate_entry(void* address, void* out_ins, size_t* out_size, void* read_address, void* write_address) {
+	if (!read_address) read_address = address;
+	if (!write_address) write_address = out_ins;
 	decoder dec;
-	uint8_t*out = (uint8_t*)out_ins;
-	size_t total_size=0;
-	void*ret_addr = address;
+	uint8_t* write_out = (uint8_t*)write_address;
+	uint8_t* code_out = (uint8_t*)out_ins;
+	size_t total_size = 0;
+	void* ret_addr = address;
 	size_t size;
-	uint8_t* dec_addr = (uint8_t*)address;
+	uint8_t* dec_addr = (uint8_t*)read_address;
+	uint8_t* code_addr = (uint8_t*)address;
 	// follow any jumps if the function starts with them
-	for (int i=0;i<10;i++) {
+	for (int i = 0; i < 10; i++) {
 		if (!dec.decode(dec_addr)) {
-			xcept("hook entry %p; failed to decode instruction at %p", address, dec_addr);
+			printf("hook entry %p; failed to decode instruction at %p (%p)", address, dec_addr, code_addr);
+			throw std::runtime_error("hook_generate_entry failed");
 		}
 		size = dec.insn_size;
 		dec_addr += size;
+		code_addr += size;
 		bool is_jmp = dec.opcode == 0xe9 || dec.opcode == 0xeb;
 		if (is_jmp) {
-			ret_addr = dec_addr + dec.op_imm[0];
+			ret_addr = code_addr + dec.op_imm[0];
 			continue;
 		} else break;
 	}
-	while (total_size<5) {
+	while (total_size < 5) {
 		total_size += size;
 
 		// oh no, a relative jump. we must patch it up
@@ -46,7 +47,7 @@ void*hook_generate_entry(void*address,void*out_ins,size_t*out_size) {
 		bool is_loop = dec.opcode == 0xe0 || dec.opcode == 0xe1 || dec.opcode == 0xe2;
 		bool is_j_cxz = dec.opcode == 0xe3;
 		if (is_jmp_short || is_jmp_near || is_call_near || is_cond_jmp_short || is_cond_jmp_near || is_loop || is_j_cxz) {
-			uint32_t addr = (uint32_t)(dec_addr + dec.op_imm[0]);
+			uint32_t addr = (uint32_t)(code_addr + dec.op_imm[0]);
 			// check that the branch is not inside the entry
 			// this routine will still generate invalid code if a function starts with, for instance, 74 00
 			// but that is extremely unlikely to happen
@@ -55,35 +56,44 @@ void*hook_generate_entry(void*address,void*out_ins,size_t*out_size) {
 				// to be sure it'll reach, we must patch them up to use 32-bit offsets
 				if (dec.op_imm_size[0] == 32) {
 					// just copy the opcode
-					memcpy(out, p, size - 4);
-					out += size - 4;
+					memcpy(write_out, p, size - 4);
+					write_out += size - 4;
+					code_out += size - 4;
 				} else if (dec.op_imm_size[0] == 16) {
 					// remove the prefix, and copy the opcode
-					memcpy(out, p + 1, size - 2 - 1); 
-					out += size - 2 - 1;
+					memcpy(write_out, p + 1, size - 2 - 1);
+					write_out += size - 2 - 1;
+					code_out += size - 2 - 1;
 				} else if (dec.op_imm_size[0] == 8) {
 					uint8_t i = *p;
-					if (is_jmp_short) *out+= 0xe9; // jmp short is 0xeb, jmp near is 0xe9
-					else if (is_cond_jmp_short) { // jcc
-						// the conditional short jmp instructions are 0x70 - 0x7f
-						// the conditional long jmp instructions are 0x80-0x8f (after the 2-byte prefix 0x0f)
-						*out++ = 0x0f;
-						*out++ = i+0x10;
+					if (is_jmp_short) {
+						*write_out++ = 0xe9; // jmp short is 0xeb, jmp near is 0xe9
+						++code_out;
+					} else if (is_cond_jmp_short) { // jcc
+												  // the conditional short jmp instructions are 0x70 - 0x7f
+												  // the conditional long jmp instructions are 0x80-0x8f (after the 2-byte prefix 0x0f)
+						*write_out++ = 0x0f;
+						++code_out;
+						*write_out++ = i + 0x10;
+						++code_out;
 					} else {
 						// fixme: replace loopne, loope, loop, jcxz and jecxz by cmp and jc instructions
-						//        haven't bothered sinced those instructions are mostly unused
-						xcept("hook entry %p: failed to convert instruction at %p from 8-bit relative branch to 32-bit", address, dec_addr);
+						//        haven't bothered since those instructions are mostly unused
+						printf("hook entry %p: failed to convert instruction at %p from 8-bit relative branch to 32-bit", address, dec_addr);
+						throw std::runtime_error("hook_generate_entry failed");
 					}
 				}
-				*(uint32_t*)out = addr - (uint32_t)(out + 4);
-				out += 4;
+				*(uint32_t*)write_out = addr - (uint32_t)(code_out + 4);
+				write_out += 4;
+				code_out += 4;
 				continue;
-			}	
+			}
 		}
-		memcpy(out, dec_addr - size, size);
-		out += size;
+		memcpy(write_out, dec_addr - size, size);
+		write_out += size;
+		code_out += size;
 
-		if (total_size<5) {
+		if (total_size < 5) {
 			bool is_jmp = dec.opcode == 0xe9 || dec.opcode == 0xeb;
 			bool is_ret = dec.opcode == 0xc2 || dec.opcode == 0xc3;
 			if (is_ret || is_jmp) {
@@ -95,132 +105,202 @@ void*hook_generate_entry(void*address,void*out_ins,size_t*out_size) {
 				}
 				if (total_size >= 5) break;
 				// oh well...
-				xcept("hook entry %p; function too small; ret or non-conditional branch found at %p", address, dec_addr);
+				printf("hook entry %p; function too small; ret or non-conditional branch found at %p (%p)", address, dec_addr, code_addr);
+				throw std::runtime_error("hook_generate_entry failed");
 			}
-			
+
 			if (!dec.decode(dec_addr)) {
-				xcept("hook entry %p; failed to decode instruction at %p", address, dec_addr);
+				printf("hook entry %p; failed to decode instruction at %p (%p)", address, dec_addr, code_addr);
+				throw std::runtime_error("hook_generate_entry failed");
 			}
 			size = dec.insn_size;
 			dec_addr += size;
+			code_addr += size;
 		}
 	}
 	// and the jump back to the real function
-	*out++ = 0xe9;
-	*(uint32_t*)out = ((uint32_t)ret_addr + total_size) - (uint32_t)(out+4);
-	out += 4;
+	*write_out++ = 0xe9;
+	++code_out;
+	*(uint32_t*)write_out = ((uint32_t)ret_addr + total_size) - (uint32_t)(code_out + 4);
+	write_out += 4;
+	++code_out;
 
-	if (out_size) *out_size = out - (uint8_t*)out_ins;
+	if (out_size) *out_size = write_out - (uint8_t*)write_address;
 
 	return ret_addr;
 }
-hook_function*hook_new(void*address,hook_proc pre,hook_proc post,int flags,int args) {
-	hook_function*f = new hook_function;
-	memset(f,0,sizeof(f));
+hook_function* hook_new(void* address, hook_proc pre, hook_proc post, int flags, int args) {
+	hook_function* f = new hook_function;
+	memset(f, 0, sizeof(f));
 	f->address = address;
 	f->pre = pre;
 	f->post = post;
 	f->flags = flags;
 	f->args = args;
-	uint8_t*p = (uint8_t*)VirtualAlloc(0,0x1000,MEM_RESERVE | MEM_COMMIT,PAGE_EXECUTE_READWRITE);
+	uint8_t* p = (uint8_t*)VirtualAlloc(nullptr, 0x1000, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	if (!p) {
+		printf("VirtualAlloc failed, error %d\n", GetLastError());
+		throw std::runtime_error("hook_new failed");
+	}
 	f->entry = p;
-	f->patch_address = hook_generate_entry(address,f->entry,&f->entry_size);
-	f->hook_code = (void*)(((uint32_t)(p+f->entry_size-1) & ~15) + 16); // align to 16 bytes
-	f->hook_code_size = hook_generate(f->hook_code,f);
+	f->patch_address = hook_generate_entry(address, f->entry, &f->entry_size);
+	f->hook_code = (void*)(((uint32_t)(p + f->entry_size - 1) & ~15) + 16); // align to 16 bytes
+	f->hook_code_size = hook_generate(f->hook_code, f);
 	return f;
 }
-void hook_delete(hook_function*f) {
-	if (f->hook_code) VirtualFree(f->hook_code,0,MEM_RELEASE);
+hook_function* hook_new_remote(HANDLE remote_process_handle, void* address, hook_proc pre, hook_proc post, int flags, int args) {
+	hook_function* f = new hook_function;
+	memset(f, 0, sizeof(f));
+	uint8_t* p = (uint8_t*)VirtualAllocEx(remote_process_handle, nullptr, 0x1000, MEM_RESERVE | MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	if (!p) {
+		printf("VirtualAllocEx failed, error %d\n", GetLastError());
+		throw std::runtime_error("hook_new_remote failed");
+	}
+	hook_function* remote_f = (hook_function*)(p + 0x1000 - sizeof(hook_function));
+	std::array<uint8_t, 0x100> read_buffer;
+	if (!ReadProcessMemory(remote_process_handle, address, read_buffer.data(), read_buffer.size(), nullptr)) {
+		printf("ReadProcessMemory failed, error %d\n", GetLastError());
+		throw std::runtime_error("hook_new_remote failed");
+	}
+	std::array<uint8_t, 0x1000> write_buffer;
+	f->address = address;
+	f->pre = pre;
+	f->post = post;
+	f->flags = flags;
+	f->args = args;
+	f->entry = p;
+	f->patch_address = hook_generate_entry(address, f->entry, &f->entry_size, read_buffer.data(), write_buffer.data() + ((uint8_t*)f->entry - p));
+	f->hook_code = (void*)(((uint32_t)(p + f->entry_size - 1) & ~15) + 16); // align to 16 bytes
+	f->hook_code_size = hook_generate(f->hook_code, remote_f, write_buffer.data() + ((uint8_t*)f->hook_code - p), f);
+	memcpy(write_buffer.data() + ((uint8_t*)remote_f - p), f, sizeof(hook_function));
+
+	if (!WriteProcessMemory(remote_process_handle, p, write_buffer.data(), write_buffer.size(), nullptr)) {
+		printf("WriteProcessMemory failed, error %d\n", GetLastError());
+		throw std::runtime_error("hook_new_remote failed");
+	}
+	return f;
+}
+void hook_delete(hook_function* f) {
+	if (f->hook_code) VirtualFree(f->hook_code, 0, MEM_RELEASE);
 	delete f;
 }
-void hook_activate(hook_function*f) {
+void hook_activate(hook_function* f) {
 	// patch up patch_address to jump to entry
 
 	DWORD oldprot;
-	if (!VirtualProtectEx(GetCurrentProcess(),f->patch_address,0x100,PAGE_EXECUTE_READWRITE,&oldprot)) {
-		//xcept("hook entry %p; VirtualProtectEx failed (error %d)",GetLastError());
-	}
-	uint8_t*out = (uint8_t*)f->patch_address;
+	VirtualProtect(f->patch_address, 0x100, PAGE_EXECUTE_READWRITE, &oldprot);
+	uint8_t* out = (uint8_t*)f->patch_address;
 	*out++ = 0xe9;
-	*(uint32_t*)out = (uint8_t*)f->hook_code - (out+4);
+	*(uint32_t*)out = (uint8_t*)f->hook_code - (out + 4);
 	out += 4;
 }
 //void hook_deactivate(hook_function*) {
 // no support yet, because i'm lazy and don't need it yet
 //}
-hook_function*hook(void*address,hook_proc pre,hook_proc post,int flags,int args) {
-	hook_function*f = hook_new(address,pre,post,flags,args);
+void hook_activate_remote(HANDLE remote_process_handle, hook_function* f) {
+	// patch up patch_address to jump to entry
+	DWORD oldprot;
+	VirtualProtectEx(remote_process_handle, f->patch_address, 0x100, PAGE_EXECUTE_READWRITE, &oldprot);
+	char buf[5];
+	uint8_t* out = (uint8_t*)buf;
+	*out++ = 0xe9;
+	*(uint32_t*)out = (uint8_t*)f->hook_code - ((uint8_t*)f->patch_address + 1 + 4);
+	out += 4;
+	if (!WriteProcessMemory(remote_process_handle, f->patch_address, buf, 5, nullptr)) {
+		printf("WriteProcessMemory failed, error %d\n", GetLastError());
+		throw std::runtime_error("hook_activate_remote failed");
+	}
+}
+hook_function* hook(void* address, hook_proc pre, hook_proc post, int flags, int args) {
+	hook_function* f = hook_new(address, pre, post, flags, args);
 	hook_activate(f);
 	return f;
 }
-size_t hook_generate(void*out,hook_function*f) {
+hook_function* hook_remote(HANDLE remote_process_handle, void* address, hook_proc pre, hook_proc post, int flags, int args) {
+	hook_function* f = hook_new_remote(remote_process_handle, address, pre, post, flags, args);
+	hook_activate_remote(remote_process_handle, f);
+	return f;
+}
+size_t hook_generate(void* out, hook_function* f, void* write_address, hook_function* read_f) {
+	if (!write_address) write_address = out;
+	if (!read_f) read_f = f;
 
-	out_buf_ptr code_buf((unsigned char*)out);
+	class code_buf_t : public out_buf {
+	public:
+		uint8_t* code_c;
+		uint8_t* write_c;
+		code_buf_t(uint8_t* code_c, uint8_t* write_c) : code_c(code_c), write_c(write_c) {}
+		virtual void puc(uint8_t x) { *write_c++ = x; ++code_c;  }
+		virtual void pus(uint16_t x) { *(uint16_t*)write_c = (uint16_t)(x); write_c += 2; code_c += 2; }
+		virtual void pui(uint32_t x) { *(uint32_t*)write_c = (uint32_t)(x); write_c += 4; code_c += 4; }
+		virtual uint32_t addr() { return (uint32_t)code_c; }
+	};
+
+	code_buf_t code_buf((uint8_t*)out, (uint8_t*)write_address);
 	codegen gen(&code_buf);
 
 	//gen.int3();
 
-	gen.add_rm_immx<32>(modrm_reg(badreg,esp),-(int)sizeof(hook_struct));
+	gen.add_rm_immx<32>(modrm_reg(badreg, esp), -(int)sizeof(hook_struct));
 
 #define rm_h(r,o,...) modrm_dispx(r,-gen.esp_val - sizeof(hook_struct) + (offsetof(hook_struct,o) __VA_ARGS__),sib_nomul(esp))
 
-	for (int i=0;i<8;i++) {
-		if (f->flags&hookflag_regs[i]) gen.mov_rm_r<32>(rm_h((reg)i,_eax,+i*4));
+	for (int i = 0; i < 8; i++) {
+		if (read_f->flags&hookflag_regs[i]) gen.mov_rm_r<32>(rm_h((reg)i, _eax, +i * 4));
 	}
 
-	gen.mov_r_rm<32>(modrm_dispx(eax,-gen.esp_val,sib_nomul(esp)));
-	gen.mov_rm_r<32>(rm_h(eax,retaddress));
+	gen.mov_r_rm<32>(modrm_dispx(eax, -gen.esp_val, sib_nomul(esp)));
+	gen.mov_rm_r<32>(rm_h(eax, retaddress));
 
-	int r=2;
-	for (int i=0;i<f->args;i++) {
-		gen.mov_r_rm<32>(modrm_dispx((reg)r,-gen.esp_val + 4 + i*4,sib_nomul(esp)));
-		gen.mov_rm_r<32>(rm_h((reg)r,arg[i]));
-		gen.mov_rm_r<32>(rm_h((reg)r,ref_arg[i]));
-		if (--r==-1) r=2;
+	int r = 2;
+	for (int i = 0; i < read_f->args; i++) {
+		gen.mov_r_rm<32>(modrm_dispx((reg)r, -gen.esp_val + 4 + i * 4, sib_nomul(esp)));
+		gen.mov_rm_r<32>(rm_h((reg)r, arg[i]));
+		gen.mov_rm_r<32>(rm_h((reg)r, ref_arg[i]));
+		if (--r == -1) r = 2;
 	}
-	if (f->pre) {
-		gen.mov_rm_imm<8>(rm_h(badreg,calloriginal),1);
-		if (-gen.esp_val==sizeof(hook_struct)) gen.mov_r_rm<32>(modrm_reg(eax,esp));
-		else gen.lea_r_rm<32>(rm_h(eax,ref_arg[0]));
+	if (read_f->pre) {
+		gen.mov_rm_imm<8>(rm_h(badreg, calloriginal), 1);
+		if (-gen.esp_val == sizeof(hook_struct)) gen.mov_r_rm<32>(modrm_reg(eax, esp));
+		else gen.lea_r_rm<32>(rm_h(eax, ref_arg[0]));
 		gen.push_imm32((uint32_t)f);
-		if (-gen.esp_val==sizeof(hook_struct)) gen.push_r32(esp);
-		else gen.push_r32(eax);
-		gen.call_rel32((uint32_t)f->pre);
-		gen.add_rm_immx<32>(modrm_reg(badreg,esp),8);
-		gen.mov_r_rm<8>(rm_h(eax,calloriginal));
-		gen.test_rm_r<8>(modrm_reg(eax,eax));
+		gen.push_r32(eax);
+		gen.call_rel32((uint32_t)read_f->pre);
+		gen.add_rm_immx<32>(modrm_reg(badreg, esp), 8);
+		gen.mov_r_rm<8>(rm_h(eax, calloriginal));
+		gen.test_rm_r<8>(modrm_reg(eax, eax));
 		gen.push_imm32(0);
-		uint32_t*retaddr = (uint32_t*)(gen.c->addr()-4);
-		for (int i=0;i<8;i++) {
-			if (f->flags&hookflag_regs[i]) gen.mov_r_rm<32>(rm_h((reg)i,_eax,+4*i));
+		uint32_t* retaddr = (uint32_t*)(code_buf.write_c - 4);
+		for (int i = 0; i < 8; i++) {
+			if (read_f->flags&hookflag_regs[i]) gen.mov_r_rm<32>(rm_h((reg)i, _eax, +4 * i));
 		}
-		gen.jnz_relx((uint32_t)f->entry);
-		gen.mov_r_rm<32>(rm_h(eax,retval));
-		gen.add_rm_immx<32>(modrm_reg(badreg,esp),4 + (f->flags&hookflag_callee_cleanup ? 4*f->args : 0));
-		while (gen.c->addr()%4) gen.nop();
+		gen.jnz_relx((uint32_t)read_f->entry);
+		gen.mov_r_rm<32>(rm_h(eax, retval));
+		gen.add_rm_immx<32>(modrm_reg(badreg, esp), 4 + (read_f->flags&hookflag_callee_cleanup ? 4 * read_f->args : 0));
+		while (gen.c->addr() % 4) gen.nop();
 		*retaddr = (uint32_t)gen.c->addr();
 	} else {
-		for (int i=0;i<8;i++) {
-			if (f->flags&hookflag_regs[i]) gen.mov_r_rm<32>(rm_h((reg)i,_eax,+4*i));
+		for (int i = 0; i < 8; i++) {
+			if (read_f->flags&hookflag_regs[i]) gen.mov_r_rm<32>(rm_h((reg)i, _eax, +4 * i));
 		}
-		gen.call_rel32((uint32_t)f->entry);
-		if (f->flags&hookflag_callee_cleanup) gen.esp_val += 4*f->args;
-	}	
-	if (f->post) {
-		gen.mov_rm_r<32>(rm_h(eax,retval));
-		if (-gen.esp_val==sizeof(hook_struct)) gen.mov_r_rm<32>(modrm_reg(eax,esp));
-		else gen.lea_r_rm<32>(rm_h(eax,ref_arg[0]));
+		gen.call_rel32((uint32_t)read_f->entry);
+		if (read_f->flags&hookflag_callee_cleanup) gen.esp_val += 4 * read_f->args;
+	}
+	if (read_f->post) {
+		gen.mov_rm_r<32>(rm_h(eax, retval));
+		if (-gen.esp_val == sizeof(hook_struct)) gen.mov_r_rm<32>(modrm_reg(eax, esp));
+		else gen.lea_r_rm<32>(rm_h(eax, ref_arg[0]));
 		gen.push_imm32((uint32_t)f);
-		if (-gen.esp_val==sizeof(hook_struct)) gen.push_r32(esp);
+		if (-gen.esp_val == sizeof(hook_struct)) gen.push_r32(esp);
 		else gen.push_r32(eax);
-		gen.call_rel32((uint32_t)f->post);
-		gen.mov_r_rm<32>(rm_h(eax,retval));
+		gen.call_rel32((uint32_t)read_f->post);
+		gen.mov_r_rm<32>(rm_h(eax, retval));
 	}
 
 #undef rm_h
-	gen.add_rm_immx<32>(modrm_reg(badreg,esp),-gen.esp_val);
-	if (f->flags&hookflag_callee_cleanup && f->args) gen.ret_imm16(f->args*4);
+	gen.add_rm_immx<32>(modrm_reg(badreg, esp), -gen.esp_val);
+	if (read_f->flags&hookflag_callee_cleanup && read_f->args) gen.ret_imm16(read_f->args * 4);
 	else gen.ret();
-	return code_buf.c - code_buf.oc;
+	return code_buf.code_c - (uint8_t*)out;
 }
 
--- sc_hook.h
+++ sc_hook.h
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <windows.h>
 
-enum {hookflag_eax=1,hookflag_ecx=2,hookflag_edx=4,hookflag_ebx=8,hookflag_esp=0x10,hookflag_ebp=0x20,hookflag_esi=0x40,hookflag_edi=0x80,
-	  hookflag_callee_cleanup=0x100};
+enum {
+	hookflag_eax = 1, hookflag_ecx = 2, hookflag_edx = 4, hookflag_ebx = 8, hookflag_esp = 0x10, hookflag_ebp = 0x20, hookflag_esi = 0x40, hookflag_edi = 0x80,
+	hookflag_callee_cleanup = 0x100
+};
 #define hookflag_reg_all (hookflag_eax|hookflag_ecx|hookflag_edx|hookflag_ebx|hookflag_ebp|hookflag_esi|hookflag_edi)
 
 #define HOOK_CDECL    (0)
@@ -23,34 +25,37 @@ struct hook_struct {
 	uint32_t _eax;
 	union {
 		uint32_t _ecx;
-		void*_this;
+		void* _this;
 	};
 	uint32_t _edx, _ebx, _esp, _ebp, _esi, _edi;
 };
 
 struct hook_function;
-typedef void (*hook_proc)(hook_struct*e,hook_function*_f);
+typedef void(*hook_proc)(hook_struct* e, hook_function* _f);
 
 struct hook_function {
 	hook_proc pre;
 	hook_proc post;
 	int flags;
 	int args;
-	int args_size;
-	void*address;
-	void*hook_code;
+	void* address;
+	void* hook_code;
 	size_t hook_code_size;
-	void*entry;
+	void* entry;
 	size_t entry_size;
-	void*patch_address;
+	void* patch_address;
+	void* user;
 };
 
-void*hook_generate_entry(void*address,void*out_ins,size_t*out_size);
-hook_function*hook_new(void*address,hook_proc pre,hook_proc post,int flags,int args);
+void* hook_generate_entry(void* address, void* out_ins, size_t* out_size, void* read_address = nullptr, void* write_address = nullptr);
+hook_function *hook_new(void* address, hook_proc pre, hook_proc post, int flags, int args);
+hook_function *hook_new_remote(HANDLE remote_process_handle, void* address, hook_proc pre, hook_proc post, int flags, int args);
 void hook_delete(hook_function*);
 void hook_activate(hook_function*);
 void hook_deactivate(hook_function*);
-hook_function*hook(void*address,hook_proc pre,hook_proc post,int flags,int args);
-size_t hook_generate(void*out,hook_function*f);
+void hook_activate_remote(HANDLE remote_process_handle, hook_function*);
+hook_function* hook(void* address, hook_proc pre, hook_proc post, int flags, int args);
+hook_function* hook_remote(HANDLE remote_process_handle, void* address, hook_proc pre, hook_proc post, int flags, int args);
+size_t hook_generate(void* out, hook_function* f, void* write_address = nullptr, hook_function* read_f = nullptr);
 
 #endif
